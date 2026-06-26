3180d415c8d6a47519ea84de705e4406446d29b2
Džana Kopić <128844494+dzankk@users.noreply.github.com>
2026-06-26 23:10:50 +0200

    add text encoder building functions to offline package

--- models/offline/__init__.py
+++ models/offline/__init__.py
@@ -1,5 +1,26 @@
 """Offline model package exports."""
 
+from .data import (
+	CTCTextEncoder,
+	OfflineHandwritingDataset,
+	build_text_encoder_for_dataset_splits,
+	build_text_encoder_for_dataset_paths,
+	create_offline_dataloader,
+	ctc_collate_fn,
+)
+from .model import CRNN
+
+__all__ = [
+	"CRNN",
+	"CTCTextEncoder",
+	"OfflineHandwritingDataset",
+	"build_text_encoder_for_dataset_splits",
+	"build_text_encoder_for_dataset_paths",
+	"create_offline_dataloader",
+	"ctc_collate_fn",
+]
+"""Offline model package exports."""
+
 from .data import CTCTextEncoder, OfflineHandwritingDataset, create_offline_dataloader, ctc_collate_fn
 from .model import CRNN
 
